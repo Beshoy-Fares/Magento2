7a798f5643fef36fe53e3808a78a86990bf1c3a5
engcom-Dash <grp-engcom-vendorworker-Dash@adobe.com>
2024-10-23 18:36:59 +0530

    32311: Revert the change

--- app/code/Magento/User/Controller/Adminhtml/User/Save.php
+++ app/code/Magento/User/Controller/Adminhtml/User/Save.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2014 Adobe
+ * All Rights Reserved.
  */
 
 namespace Magento\User\Controller\Adminhtml\User;
@@ -34,9 +34,9 @@ class Save extends \Magento\User\Controller\Adminhtml\User implements HttpPostAc
     {
         if (!($this->securityCookie instanceof SecurityCookie)) {
             return \Magento\Framework\App\ObjectManager::getInstance()->get(SecurityCookie::class);
-        } else {
-            return $this->securityCookie;
         }
+
+        return $this->securityCookie;
     }
 
     /**
